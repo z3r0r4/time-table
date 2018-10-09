325e7cc960c082bee8d63463a11f89793fd32dca
unknown <R4C1.meru@protonmail.com>
2018-10-09 22:35:35 +0200

    Added Tabs

--- app/src/main/java/com/zero/zero/timetable/MainActivity.java
+++ app/src/main/java/com/zero/zero/timetable/MainActivity.java
@@ -1,22 +1,54 @@
 package com.zero.zero.timetable;
 
+        import android.support.design.widget.TabLayout;
+        import android.support.v4.app.FragmentActivity;
+        import android.support.v4.view.ViewPager;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
         import android.webkit.WebView;
+        import android.widget.TableLayout;
 
         import OVP.Display;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //necessary
 
+        Log.d(TAG,"onCreate: Starting.");
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
-        WebView TimeTable1 = (WebView) findViewById(R.id.Table1);
-        Display.ViewOVP(TimeTable1);
+        //Setting up the Viewpager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+       TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+       // WebView TimeTable1 = (WebView) findViewById(R.id.Table1);
+      //  Display.ViewOVP(TimeTable1);
 
     }
 
+
+
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "TAB1");
+        adapter.addFragment(new Tab2Fragment(), "TAB2");
+        adapter.addFragment(new Tab3Fragment(), "TAB3");
+        viewPager.setAdapter(adapter);
+    }
 }
---
+++ app/src/main/java/com/zero/zero/timetable/Tab1Fragment.java
@@ -0,0 +1,32 @@
+package com.zero.zero.timetable;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Tab1Fragment extends Fragment {
+    private static final String TAG = "Tab1Fragment";
+    private Button btnTest;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.tab1_fragment,container,false);
+    btnTest = (Button) view.findViewById(R.id.btnTEST1);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+return view;
+    }
+}
