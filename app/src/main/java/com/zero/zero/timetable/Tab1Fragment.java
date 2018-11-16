b2d4285959b368b96b44d30102e4ec55cfefd5dc
unknown <R4C1.meru@protonmail.com>
2018-11-16 23:31:42 +0100

    removed hardcoded strings, added login mask

--- app/src/main/java/com/zero/zero/timetable/Tab1Fragment.java
+++ app/src/main/java/com/zero/zero/timetable/Tab1Fragment.java
@@ -7,25 +7,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import OVP.Display;
+
 public class Tab1Fragment extends Fragment {
+
     private static final String TAG = "Tab1Fragment";
-    private Button btnTest;
+//    private Button btnTest;
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.tab1_fragment,container,false);
-    btnTest = (Button) view.findViewById(R.id.btnTEST1);
 
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
-            }
-        });
+
+        View view = inflater.inflate(R.layout.tab1_fragment,container,false);
+
+        WebView TimeTable1 = (WebView) view.findViewById(R.id.Table1);
+        Display.ViewOVP(TimeTable1,1);
+
+//
+//    btnTest = (Button) view.findViewById(R.id.btnTEST1);
+//
+//        btnTest.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 return view;
     }
--- app/src/main/java/com/zero/zero/timetable/Tab2Fragment.java
+++ app/src/main/java/com/zero/zero/timetable/Tab2Fragment.java
@@ -7,26 +7,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import OVP.Display;
+
 public class Tab2Fragment extends Fragment {
     private static final String TAG = "Tab2Fragment";
-    private Button btnTest;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.tab2_fragment,container,false);
-    btnTest = (Button) view.findViewById(R.id.btnTEST2);
+        @Nullable
+        @Override
+        public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
 
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 2",Toast.LENGTH_SHORT).show();
-            }
-        });
+            View view = inflater.inflate(R.layout.tab2_fragment, container, false);
 
-return view;
+            WebView TimeTable2 = (WebView) view.findViewById(R.id.Table2);
+            Display.ViewOVP(TimeTable2, 2);
+
+            return view;
+        }
     }
-}
+
