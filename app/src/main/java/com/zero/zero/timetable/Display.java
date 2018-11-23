e3d2d31eb1860841e4993beca535e6cf83cd01b0
unknown <R4C1.meru@protonmail.com>
2018-11-24 00:18:30 +0100

    reAdded Tabs, this time inside of a drawer

---
+++ app/src/main/java/com/zero/zero/timetable/Display.java
@@ -0,0 +1,52 @@
+package com.zero.zero.timetable;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.zero.zero.timetable.MainActivity;
+import com.zero.zero.timetable.R;
+
+public class Display{
+    private static final String TAG = "TTDisplay";
+    private static String OVP_link = "";
+
+
+    public static void ViewOVP(WebView TimeTable, String LoginData, int i){
+
+        Log.d(TAG,LoginData);
+        switch (i){
+            case 1:
+                 OVP_link = LoginData+"1.htm";
+                break;
+            case 2:
+                OVP_link =  LoginData+"2.htm";
+                break;
+            case 3:
+                OVP_link ="";
+                break;
+            default:
+                OVP_link = "";
+                break;
+        }
+
+
+
+        TimeTable.loadUrl(OVP_link);
+
+        //set interaction environment variables and things
+        final boolean Interaction_enabler = true;
+        TimeTable.setInitialScale(100);
+        TimeTable.getSettings().setBuiltInZoomControls(Interaction_enabler);
+        TimeTable.setWebViewClient(new WebViewClient(){  //just disables the redirection to hyperlinks
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return true;
+            }
+        });
+    }
+}
