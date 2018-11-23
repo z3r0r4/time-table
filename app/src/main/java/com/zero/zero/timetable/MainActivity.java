d1edfcd5af1690e8759c5cc997f536efc1b6d6df
unknown <R4C1.meru@protonmail.com>
2018-11-23 22:21:12 +0100

    added a NavigationDrawer

--- app/src/main/java/com/zero/zero/timetable/MainActivity.java
+++ app/src/main/java/com/zero/zero/timetable/MainActivity.java
@@ -1,26 +1,30 @@
 package com.zero.zero.timetable;
 
-        import android.support.design.widget.TabLayout;
-        import android.support.v4.app.DialogFragment;
-        import android.support.v4.app.Fragment;
-        import android.support.v4.app.FragmentActivity;
-        import android.support.v4.view.ViewPager;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 
-        import android.os.Bundle;
-        import android.util.Log;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
-        import com.zero.zero.timetable.Login.LoginDialogFragment;
-        import com.zero.zero.timetable.Tabs.SectionsPageAdapter;
-        import com.zero.zero.timetable.Tabs.Tab1Fragment;
-        import com.zero.zero.timetable.Tabs.Tab2Fragment;
-        import com.zero.zero.timetable.Tabs.Tab3Fragment;
-        import com.zero.zero.timetable.Tabs.TabFragmentActivity;
+import com.zero.zero.timetable.Login.LoginDialogFragment;
+import com.zero.zero.timetable.Tabs.SectionsPageAdapter;
+import com.zero.zero.timetable.Tabs.Tab1Fragment;
+import com.zero.zero.timetable.Tabs.Tab2Fragment;
+import com.zero.zero.timetable.Tabs.Tab3Fragment;
+import com.zero.zero.timetable.Tabs.TabFragmentActivity;
 
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
     private SectionsPageAdapter mSectionsPageAdapter;
 
     private ViewPager mViewPager;
@@ -41,35 +45,58 @@ public class MainActivity extends FragmentActivity {
     // or substitute lessons
     //
     //future: add controller for grades and homework and plan for exams
-
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //necessary
-        Log.d(TAG,"onCreate: Starting.");
-        Log.d(TAG,"onCreate: calling Logincheck.");
+        //cause the original tolbar was removed
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        showLogin();
+        drawer = findViewById(R.id.drawer_layout);
 
-        //put into own class and stuff
-        Log.d(TAG,"onCreate: Initializing Tabs.");
-        //Setting a sections adapter to add tabs into
-        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-      //Setting up the Viewpager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        setupViewPager(mViewPager);
-        tabLayout.setupWithViewPager(mViewPager);
+       // this conncets the cute button at top to the action of opening the drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+//        Log.d(TAG, "onCreate: Starting.");
+//
+//        Log.d(TAG, "onCreate: calling Logincheck.");
+//        showLogin();
+//
+//
+//        //put into own class and stuff
+//        Log.d(TAG, "onCreate: Initializing Tabs.");
+//        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager()); //Setting a sections adapter to add tabs into
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+//        mViewPager = (ViewPager) findViewById(R.id.container);   //Setting up the Viewpager with the sections adapter.
+//        setupViewPager(mViewPager);
+//        tabLayout.setupWithViewPager(mViewPager);
+
 
     }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+        super.onBackPressed();
+    }
+
     public void showLogin() {
         DialogFragment newFragment = new LoginDialogFragment();
         newFragment.show(getSupportFragmentManager(), "login");
     }
 
     //put into own class and stuff
-    private void setupViewPager(ViewPager viewPager){
+    private void setupViewPager(ViewPager viewPager) {
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
         adapter.addFragment(new Tab1Fragment(), "Gestern");
         adapter.addFragment(new Tab2Fragment(), "Heute");
