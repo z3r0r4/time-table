722d30cb6390264f1c4c9ffe0b03a4bfe0bded37
unknown <R4C1.meru@protonmail.com>
2018-12-15 19:07:26 +0100

    improved MTT-Layout further / added setLesson function

--- app/src/main/java/com/zero/zero/timetable/MyTimeTableManagement/MyTimeTableFragment.java
+++ app/src/main/java/com/zero/zero/timetable/MyTimeTableManagement/MyTimeTableFragment.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,17 +16,18 @@ import android.widget.TextView;
 import com.zero.zero.timetable.MainActivity;
 import com.zero.zero.timetable.R;
 
-import java.util.ArrayList;
-
 //import receive.HTMLFetcher;
 
 
 public class MyTimeTableFragment extends Fragment {
     private final static String TAG = "MyTimeTableFragment";
     private static View viewTimetable = null;
-    private int NumberOfLessonsPerDay = 11;
     private TableLayout tableLayout = null;
-    private ArrayList<TableRow> tableRowsLessons = null;
+
+
+    private int NumberOfLessonsPerDay = 11;
+    private TableRow[] tableRowsLessons = null;
+
     private TableRow tableRowHeader = null;
     private TextView[] mTextViewsHeader = null;
     private String[] mStringsHeader = {"Stunde", "Klasse", "Kurs", "Raum", "Art", "Info"};
@@ -43,39 +43,45 @@ public class MyTimeTableFragment extends Fragment {
         tableLayout = viewTimetable.findViewById(R.id.TableLayoutTT);
         tableLayout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
 
-        //add a row for every lesson of the day and add a numbering
-        tableRowsLessons = new ArrayList<>();
-        for (int i = 0; i < NumberOfLessonsPerDay; i++) {
-            tableRowsLessons.add(new TableRow(getActivity()));
+
+        //add a indexing first line
+        tableRowHeader = new TableRow(getActivity());
+        mTextViewsHeader = new TextView[mStringsHeader.length - 1];
+//        for (TextView textView : mTextViewsHeader) {//doesnt work because tableRow is not a pointer but just a temp copy
+        for (int i = 0; i < mTextViewsHeader.length; i++) {
+            mTextViewsHeader[i] = new TextView(getActivity());
+            mTextViewsHeader[i].setPadding(10, 10, 10, 10);
+            mTextViewsHeader[i].setText(mStringsHeader[i]);
+
+            tableRowHeader.addView(mTextViewsHeader[i]);
+        }
+        tableLayout.addView(tableRowHeader);
+
+
+        //add a row for every lesson of the day and add a numbering TextView
+        tableRowsLessons = new TableRow[NumberOfLessonsPerDay];
+//        for (TableRow tableRow : tableRowsLessons) {//doesnt work because tableRow is not a pointer but just a temp copy
+        for (int i = 0; i < tableRowsLessons.length; i++) {
+            tableRowsLessons[i] = new TableRow(getActivity());
 
             TextView TextViewLessonNumber = new TextView(getActivity());
             TextViewLessonNumber.setText(i + 1 + ". Stunde");
-            tableRowsLessons.get(i).addView(TextViewLessonNumber);
-        }
-        //add a indexing first line
-        tableRowHeader = new TableRow(getActivity());
-        tableRowHeader.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
-        mTextViewsHeader = new TextView[mStringsHeader.length - 1];
-        int i = 0;
-        for (TextView textView : mTextViewsHeader) {
-            textView = new TextView(getActivity());
+            TextViewLessonNumber.setPadding(10, 10, 10, 10);
 
-            textView.setPadding(10, 10, 10, 10);
-            textView.setText(mStringsHeader[i]);
-
-            textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 110));
-            textView.setGravity(Gravity.START);
-            tableRowHeader.addView(textView);
-            i++;
+            tableRowsLessons[i].addView(TextViewLessonNumber);
+            tableLayout.addView(tableRowsLessons[i]);
         }
 
-        tableLayout.addView(tableRowHeader);
-        //add all rows to the TableLayout
-        for (TableRow tableRow : tableRowsLessons) {
-            tableLayout.addView(tableRow);
-        }
-
+        setLesson(1, new String[]{"a", "e", "o"});
 
         return viewTimetable;
+    }
+
+    public void setLesson(int LessonNumber, String[] ScheduleEntry) {
+        for (String s : ScheduleEntry) {
+            TextView textView = new TextView(getActivity());
+            textView.setText(s);
+            tableRowsLessons[LessonNumber - 1].addView(textView);
+        }
     }
 }
